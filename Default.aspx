d58fc8d2b3e54bcd42ff34301000ce5c513e4347
Alan Jackson <alandjackson@gmail.com>
2013-05-30 14:27:45 -0500

    Initial commit.

---
+++ Default.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebFormsAsyncResultTest._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="Scripts/jquery-1.7.1.min.js" type="text/javascript"></script>
+    <script language="javascript" type="text/javascript">
+        $(document).ready(function () {
+            <%= Session["result"] != null ? "CheckStatus();" : "" %>
+        });
+        function CheckStatus() {
+            $("#WaitText").text($("#WaitText").text() + ".");
+            <%= ClientScript.GetCallbackEventReference(this,"", "ShowResult", null) %>;
+        }
+        function ShowResult(eventArgument, context) {
+            if (eventArgument == "SUCCESS") {
+                $("#Wait").hide();
+                $("#DoneSuccess").show();
+                window.location.assign("/Default.aspx?GetResult=1");
+            } else if (eventArgument == "WORKING") {
+                setTimeout("CheckStatus();", 1000);
+            } else if (eventArgument == "UNKNOWN") {
+                $("#WaitText").text("UNKNOWN STATUS");
+            } else {
+                $("#Wait").hide();
+                $("#DoneErrorMsg").html(eventArgument);
+                $("#DoneError").show();
+            }
+        }
+    </script>
+
+
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <div>
+            <span>Do something long: </span>
+            <asp:Button runat="server" Text="Start" ID="StartButton" OnClick="StartButton_Click" />
+        </div>
+
+        <asp:Panel runat="server" ID="WorkerProgressPanel" Visible="false">
+
+            <div id="Wait">
+                <span id="WaitText">Working.</span></div>
+            <div id="DoneSuccess" style="display:none;">
+                <strong>Success!</strong> Process Finished.  
+            </div>
+            <div id="DoneError" style="display:none;">
+                <strong>Error: </strong> 
+                <label id="DoneErrorMsg"></label>
+            </div>
+
+        </asp:Panel>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormsAsyncResultTest
+{
+    public partial class _Default : System.Web.UI.Page, ICallbackEventHandler
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (! IsPostBack && ! string.IsNullOrEmpty(Request.Params["GetResult"]))
+            {
+                ReturnResultsFile();
+            }
+        }
+
+        protected void StartButton_Click(object sender, EventArgs e)
+        {
+            Session["result"] = BeginWork(5);
+            WorkerProgressPanel.Visible = true;
+        }
+
+        public string GetCallbackResult()
+        {
+            var res = (IAsyncResult)Session["result"];
+            if (res == null)
+                return "UNKNOWN";
+
+            if (!res.IsCompleted)
+                return "WORKING";
+
+            // async process is finished so clear the session
+            Session["result"] = null;
+
+            // check for error
+            if (!string.IsNullOrEmpty((string)Session["ResultException"]))
+            {
+                string errorMsg = (string)Session["ResultException"];
+                Session["ResultException"] = null;
+                return errorMsg;
+            }
+            return "SUCCESS";
+        }
+
+        public void RaiseCallbackEvent(string args)
+        {
+        }
+
+        protected delegate void BeginWorkDelegate(int seconds);
+        protected IAsyncResult BeginWork(int seconds)
+        {
+            return new BeginWorkDelegate(DoWork).BeginInvoke(seconds, WorkCallback, null);
+        }
+
+        protected void WorkCallback(IAsyncResult iresult)
+        {
+            ((iresult as AsyncResult).AsyncDelegate as BeginWorkDelegate).EndInvoke(iresult);
+        }
+
+        protected void DoWork(int seconds)
+        {
+            try
+            {
+                System.Threading.Thread.Sleep(seconds * 1000);
+                Session["ResultBytes"] = System.IO.File.ReadAllBytes(Server.MapPath("~/file.xls"));
+                Session["ResultAttachmentName"] = "file.xls";
+            }
+            catch (Exception e)
+            {
+                Session["ResultException"] = e.ToString();
+            }
+        }
+
+        protected void ReturnResultsFile()
+        {
+            byte[] responseBuffer = (byte[])Session["ResultBytes"];
+            Session["ResultBytes"] = null;
+
+            string responseAttachmentName = (string) Session["ResultAttachmentName"];
+
+            if (responseBuffer == null || responseAttachmentName == null)
+                throw new Exception("Result file not found");
+
+            // Return the workbook to the stream
+            Response.Clear();
+            Response.Buffer = true;
+            Response.AddHeader("Content-Disposition", String.Format("attachment;filename={0}", responseAttachmentName));
+            Response.AddHeader("Content-Length", responseBuffer.Length.ToString());
+            Response.BinaryWrite(responseBuffer);
+            Response.End();
+        }
+
+
+    }
+}
